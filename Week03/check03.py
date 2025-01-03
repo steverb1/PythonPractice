32325e3f4bfe21422ad4e58a369eb0d25b5d513a
steverb1 <steverb@mindspring.com>
2025-01-03 11:52:54 -0700

    Week 3 check

---
+++ Week03/check03.py
@@ -0,0 +1,11 @@
+age = int(input('How old are you? '))
+print(f'On your next birthday, you will be {age + 1}')
+print()
+
+cartons = int(input('How many egg cartons do you have? '))
+print(f'You have {cartons * 12} eggs')
+print()
+
+cookies = int(input('How many cookies do you have? '))
+people = int(input('How many people are there? '))
+print(f'Each person may have {cookies / people} cookies')
