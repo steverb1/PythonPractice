f7f49d1809f4cbd4da779b7e94f61690c44df273
steverb1 <steverb@mindspring.com>
2025-01-06 07:09:18 -0700

    renaming check, first pass at teach

---
+++ Week04/teach04.py
@@ -0,0 +1,15 @@
+import math
+
+
+print('Welcome to the velocity calculator. Please enter the following:')
+print()
+m = float(input('Mass (in kg): '))
+g = float(input('Gravity (in m/s^2, 9.8 for Earth, 24 for Jupiter): '))
+t = int(input('Time (in seconds): '))
+p = float(input('Density of the fluid (in kg/m^3, 1.3 for air, 1000 for water): '))
+A = float(input('Cross sectional area (in m^2): '))
+C = float(input('Drag constant (0.5 for sphere, 1.1 for cylinder): '))
+
+c = .5 * p * A * C
+velocity = math.sqrt(m * g / c) * (1 - math.exp((-math.sqrt(m * g * c) / m) * t))
+print(f'The velocity after {t} seconds is: {velocity:.3f} m/s')
