8640c3d0754fd5ef1ce52c9dc5965ecefe3e95c9
steverb1 <steverb@mindspring.com>
2025-01-07 13:09:01 -0700

    Updated test names.

--- Week06/test_check06.py
+++ Week06/test_check06.py
@@ -2,22 +2,22 @@ from check06 import determine_loan_approval
 
 
 class Test_Check06:
-    def test_scenario1(self):
+    def test_check06_scenario1(self):
         approved = determine_loan_approval(8, 7, 8, 1)
         assert approved == True
 
-    def test_scenario2(self):
+    def test_check06_scenario2(self):
         approved = determine_loan_approval(5, 2, 7, 5)
         assert approved == True
 
-    def test_scenario3(self):
+    def test_check06_scenario3(self):
         approved = determine_loan_approval(8, 2, 8, 3)
         assert approved == False
 
-    def test_scenario4(self):
+    def test_check06_scenario4(self):
         approved = determine_loan_approval(2, 4, 1, 7)
         assert approved == True
 
-    def test_scenario5(self):
+    def test_check06_scenario5(self):
         approved = determine_loan_approval(4, 5, 5, 3)
         assert approved == False
