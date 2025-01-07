d7ee305a1996c8dc8049072f861db2518907da70
steverb1 <steverb@mindspring.com>
2025-01-07 08:10:25 -0700

    Completed check05

---
+++ Week05/check05.py
@@ -0,0 +1,26 @@
+first_number = int(input('What is the first number? '))
+second_number = int(input('What is the second number? '))
+
+if first_number > second_number:
+    print(f'The first number is greater')
+else:
+    print(f'The first number is not greater')
+
+if first_number == second_number:
+    print(f'The numbers are equal')
+else:
+    print(f'The numbers are not equal')
+
+if second_number > first_number:
+    print(f'The second number is greater')
+else:
+    print(f'The second number is not greater')
+
+my_favorite_animal = 'snake'
+
+user_favorite_animal = input('What is your favorite animal? ')
+
+if my_favorite_animal == user_favorite_animal.lower():
+    print('That\'s my favorite animal too!')
+else:
+    print('That one is not my favorite.')
