97845d8486ebbd51cdf0baff2fd5b203583b1aa0
steverb1 <steverb@mindspring.com>
2025-01-09 13:45:08 -0700

    Fixing bug.

--- Week06/teach06.py
+++ Week06/teach06.py
@@ -8,7 +8,7 @@ def can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second
         if has_golden_passport2:
             rider2_age = 18
 
-        if rider1_height <= 36 or rider2_height <= 36:
+        if rider1_height < 36 or rider2_height < 36:
             can_ride = False
         elif rider1_age >= 18 and rider1_height >= 36 and rider2_height >= 36:
             can_ride = True
