ecf19fff7843396385b500c58dbc23ec47b44bce
steverb1 <steverb@mindspring.com>
2025-01-09 11:00:40 -0700

    Added tests for stretch rules.  Can't get 100% coverage.

--- Week06/test_teach06.py
+++ Week06/test_teach06.py
@@ -55,3 +55,72 @@ class Test_Teach06:
         has_golden_passport2 = False
         can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider, rider2_age, rider2_height, has_golden_passport2)
         assert can_ride == True
+
+    def test_stretch_rule1_not_18_can_ride(self):
+        rider1_age = 17
+        has_golden_passport1 = False
+        rider1_height = 52
+        have_second_rider = True
+        rider2_age = 17
+        rider2_height = 52
+        can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider, rider2_age, rider2_height)
+        assert can_ride == True
+
+    def test_stretch_rule1_too_young_cannot_ride(self):
+        rider1_age = 17
+        has_golden_passport1 = False
+        rider1_height = 52
+        have_second_rider = True
+        rider2_age = 11
+        rider2_height = 52
+        can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider, rider2_age, rider2_height)
+        assert can_ride == False
+
+    def test_stretch_rule1_too_short_cannot_ride(self):
+        rider1_age = 17
+        has_golden_passport1 = False
+        rider1_height = 51
+        have_second_rider = True
+        rider2_age = 17
+        rider2_height = 52
+        can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider, rider2_age, rider2_height)
+        assert can_ride == False
+
+    def test_stretch_rule2_single_rider_too_short_cannot_ride(self):
+        rider1_age = 12
+        has_golden_passport1 = True
+        rider1_height = 52
+        have_second_rider = False
+        can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider)
+        assert can_ride == False
+
+    def test_stretch_rule2_single_rider_tall_enough_can_ride(self):
+        rider1_age = 12
+        has_golden_passport1 = True
+        rider1_height = 62
+        have_second_rider = False
+        can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider)
+        assert can_ride == True
+
+    def test_stretch_rule2_double_riders_can_ride(self):
+        rider1_age = 17
+        has_golden_passport1 = False
+        rider1_height = 52
+        have_second_rider = True
+        rider2_age = 12
+        rider2_height = 52
+        has_golden_passport2 = True
+        can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider, rider2_age, rider2_height, has_golden_passport2)
+        assert can_ride == True
+
+    # This is passing by accident. Without any guidance on height, stretch rule 3 is already covered by stretch rule 1.
+    def test_stretch_rule3_double_riders_can_ride(self):
+        rider1_age = 16
+        has_golden_passport1 = False
+        rider1_height = 52
+        have_second_rider = True
+        rider2_age = 14
+        rider2_height = 52
+        has_golden_passport2 = False
+        can_ride = can_riders_ride(rider1_age, rider1_height, has_golden_passport1, have_second_rider, rider2_age, rider2_height, has_golden_passport2)
+        assert can_ride == True
