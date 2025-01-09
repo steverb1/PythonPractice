39a032bcefb59d14c8ac453dc2328a805b20f11e
steverb1 <steverb@mindspring.com>
2025-01-09 11:34:08 -0700

    Use boolean parameters in method call.

--- Week06/burton_teach06.py
+++ Week06/burton_teach06.py
@@ -6,13 +6,13 @@ Purpose: Amusement park ride requirements.
 """
 
 def can_riders_ride(age1, height1, golden1, is_second_rider, age2 = None, height2 = None, golden2 = None):
-    if is_second_rider.lower() == "yes":
+    if is_second_rider:
         # Rule 1
         if height1 < 36 or height2 < 36:
             can_ride = False
         else:
             # Rule 3
-            if age1 >= 18 or age2 >= 18 or golden1.lower() == "yes" or golden2.lower() == "yes":
+            if age1 >= 18 or age2 >= 18 or golden1 or golden2:
                 # At least one is an adult
                 can_ride = True
             else:
@@ -28,7 +28,7 @@ def can_riders_ride(age1, height1, golden1, is_second_rider, age2 = None, height
                     can_ride = False
     else: # There is only one rider
         # Rule 2
-        if (age1 >= 18 or golden1.lower() == "yes") and height1 >= 62:
+        if (age1 >= 18 or golden1) and height1 >= 62:
             can_ride = True
         else:
             can_ride = False
@@ -46,15 +46,23 @@ height1 = int(input("What is the height of the first rider? "))
 
 if age1 >= 12 and age1 < 18:
     golden1 = input("Does this rider have a golden passport (yes/no)? ")
+    if golden1.lower() == 'yes':
+        golden1 = True
 
 is_second_rider = input("Is there a second rider (yes/no)? ")
-
 if is_second_rider.lower() == "yes":
+    is_second_rider = True
+else:
+    is_second_rider = False
+
+if is_second_rider:
     age2 = int(input("What is the age of the second rider? "))
     height2 = int(input("What is the height of the second rider? "))
 
     if age2 >= 12 and age2 < 18:
         golden2 = input("Does this rider have a golden passport (yes/no)? ")
+        if golden2.lower() == 'yes':
+            golden2 = True
 
 can_ride = can_riders_ride(age1, height1, golden1, is_second_rider, age2, height2, golden2)
 
