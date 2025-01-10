b1df4566d66c76b75c73268c9a3ff848916fc12e
steverb1 <steverb@mindspring.com>
2025-01-10 11:45:18 -0700

    Created check08

---
+++ Week08/check08.py
@@ -0,0 +1,12 @@
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+print()
+
+for i in range(1, 9):
+    print(i)
+print()
+
+for i in range(2, 22, 2):
+    print(i)
