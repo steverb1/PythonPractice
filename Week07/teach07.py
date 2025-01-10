2737e8602284c0345f9935eadf904062425b8e2b
steverb1 <steverb@mindspring.com>
2025-01-10 08:06:32 -0700

    Added stretch challenge.

--- Week07/teach07.py
+++ Week07/teach07.py
@@ -1,14 +1,20 @@
 import random
 
+play_again = 'yes'
 
-magic_number = random.randint(1, 100)
-guess = -1
+while play_again == 'yes':
+    magic_number = random.randint(1, 100)
+    guess = -1
+    number_of_guesses = 0
 
-while guess != magic_number:
-    if guess < magic_number:
-        print("Higher")
-    elif guess > magic_number:
-        print("Lower")
-    guess = int(input("What is your guess? "))
+    while guess != magic_number:
+        guess = int(input("What is your guess? "))
+        if guess < magic_number:
+            print("Higher")
+        elif guess > magic_number:
+            print("Lower")
+        number_of_guesses += 1
+        
+    print(f"You guessed it in {number_of_guesses} guesses!")
 
-print("You guessed it!")
+    play_again = input("Do you want to play again? ")
