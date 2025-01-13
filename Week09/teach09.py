cc147bf830b12dea5fab6d24dd72734bf3c37257
steverb1 <steverb@mindspring.com>
2025-01-13 13:12:37 -0700

    Stretch challenge

--- Week09/teach09.py
+++ Week09/teach09.py
@@ -9,3 +9,7 @@ while True:
 print("The sum is: ", sum(numbers))
 print("The average is: ", sum(numbers) / len(numbers))
 print("The largest number is: ", max(numbers))
+
+print("The smallest positive number is: ", min([x for x in numbers if x > 0]))
+
+print("The sorted list is: ", sorted(numbers))
