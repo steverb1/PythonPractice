6420527759be22043024d886989fd728759d0e89
steverb1 <steverb@mindspring.com>
2025-01-14 12:54:28 -0700

    Core requirements

---
+++ Week10/teach10.py
@@ -0,0 +1,18 @@
+print('Enter the names and balances of bank accounts (type: quit when done)')
+
+account_names = []
+account_balances = []
+
+name = ''
+
+while name != 'quit':
+    name = input('What is the name of the account? ')
+    if name != 'quit':
+        balance = float(input('What is the balance? '))
+        account_names.append(name)
+        account_balances.append(balance)
+
+print()
+print('Account Information:')
+for i in range(len(account_names)):
+    print(f'{account_names[i]} - ${account_balances[i]:.2f}')
