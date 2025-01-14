df3dc18f69570ce968098bfe724bb088f781438f
steverb1 <steverb@mindspring.com>
2025-01-14 13:59:16 -0700

    First stretch requirement.

--- Week10/teach10.py
+++ Week10/teach10.py
@@ -17,5 +17,9 @@ print('Account Information:')
 for i in range(len(account_names)):
     print(f'{account_names[i]} - ${account_balances[i]:.2f}')
 
+print()
 print(f'Total: ${sum(account_balances):.2f}')
 print(f'Average: ${sum(account_balances) / len(account_balances):.2f}')
+
+highest_index = account_balances.index(max(account_balances))
+print(f'Highest balance: {account_names[highest_index]} - ${account_balances[highest_index]:.2f}')
