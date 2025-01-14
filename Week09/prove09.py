4502f19a52359d1ce866c1ed7e8ee6955ca6abf0
steverb1 <steverb@mindspring.com>
2025-01-14 09:48:16 -0700

    Final requirements.

--- Week09/prove09.py
+++ Week09/prove09.py
@@ -1,4 +1,5 @@
 items = []
+prices = []
 
 print("Welcome to the Shopping Cart Program!")
 
@@ -16,11 +17,24 @@ while True:
     if action == 1:
         item_name = input("What item would you like to add? ")
         items.append(item_name)
+        item_price = float(input(f"What is the price of '{item_name}'? "))
+        prices.append(item_price)
         print(f"'{item_name}' has been added to the cart.")
     if action == 2:
         print("The contents of the shopping cart are:")
-        for item in items:
-            print(item)
+        for i in range(len(items)):
+            print(f'{i + 1}. {items[i]} - ${prices[i]:.2f}')
+    if action == 3:
+        remove_index = int(input('Which item would you like to remove? ')) - 1
+        if remove_index < 0 or remove_index >= len(items):
+            print(f'Item number {remove_index + 1} does not exist.')
+            continue
+        items.pop(remove_index)
+        prices.pop(remove_index)
+        print('Item removed.')
+    if action == 4:
+        total = sum(prices)
+        print(f"The total price of the items in the shopping cart is ${total:.2f}")
     if action == 5:
         print("Thank you. Goodbye.")
         break
