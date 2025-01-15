994624d8b3a3ada79a1998429025c450158430e9
steverb1 <steverb@mindspring.com>
2025-01-15 10:56:50 -0700

    Stretch work for teach11

--- Week11/teach11.py
+++ Week11/teach11.py
@@ -1,4 +1,11 @@
 with open('hr_system.txt') as file:
     for line in file:
-        line = line.split()
-        print(f'Name: {line[0]}, Title: {line[2]}')
+        fields = line.split()
+        name = fields[0]
+        id = fields[1]
+        title = fields[2]
+        salary = fields[3]
+        monthly_pay = float(salary) / 24
+        if title == 'Engineer':
+            monthly_pay += 1000
+        print(f'{name} (ID: {id}), {title} - ${monthly_pay:.2f}')
