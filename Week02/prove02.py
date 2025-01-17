1e3b23136cb18b22fcf0322148524f52bc76c093
steverb1 <steverb@mindspring.com>
2025-01-17 07:13:24 -0700

    Put back in user interaction, for running without unit tests.

--- Week02/prove02.py
+++ Week02/prove02.py
@@ -4,3 +4,14 @@ def build_story(adjective, animal, verb1, exclamation, verb2, verb3):
             'Miraculously, that caused it to stop, but not before it tried to {} right in front of my family.'
 
     return story.format(adjective, animal, verb1, exclamation.capitalize(), verb2, verb3)
+
+if __name__ == '__main__':
+    print('Please enter the following:\n')
+    adjective = input('adjective: ')
+    animal = input('animal: ')
+    verb1 = input('verb: ')
+    exclamation = input('exclamation: ')
+    verb2 = input('verb: ')
+    verb3 = input('verb: ')
+    
+    print(build_story(adjective, animal, verb1, exclamation.capitalize(), verb2, verb3))
