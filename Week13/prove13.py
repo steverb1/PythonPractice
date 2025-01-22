281d44c6ff9affeaaf91ebe1e59439aafa316a41
steverb1 <steverb@mindspring.com>
2025-01-22 08:34:46 -0700

    For prove13 added UI

--- Week13/prove13.py
+++ Week13/prove13.py
@@ -1,6 +1,5 @@
-def compute_windchills(temperature, scale):
-    if scale == 'S':
-        temperature = convert_celsius_to_fahrenheit(temperature)
+def compute_windchills(temperature):
+    
     windspeed = 5
     windspeeds = []
     while windspeed <= 60:
@@ -11,3 +10,17 @@ def compute_windchills(temperature, scale):
 
 def convert_celsius_to_fahrenheit(celsius):
     return round(celsius * 9 / 5 + 32, 2)
+
+if __name__ == '__main__':
+    temperature = float(input("What is the temperature? "))
+    scale = input("Fahrenheit or Celsius (F/C)? ").upper()
+
+    if scale == 'C':
+        temperature = convert_celsius_to_fahrenheit(temperature)
+    
+    windchills = compute_windchills(temperature)
+    
+    windspeed = 5
+    for windchill in windchills:
+        print(f"At temperature {temperature}F, and windspeed {windspeed} mph, the windchill is: {windchill}F")
+        windspeed += 5
--- Week13/test_prove13.py
+++ Week13/test_prove13.py
@@ -4,17 +4,15 @@ from prove13 import convert_celsius_to_fahrenheit
 class Test_Prove:
     def test_prove_fahrenheit(self):
         temperature = 8
-        scale = 'F'
 
-        windchills = compute_windchills(temperature, scale)
+        windchills = compute_windchills(temperature)
 
         assert windchills == [-1.11, -6.02, -9.15, -11.50, -13.40, -15.00, -16.39, -17.62, -18.73, -19.74, -20.67, -21.53]
 
     def test_prove_celsius(self):
-        temperature = -10
-        scale = 'S'
+        temperature = 14
 
-        windchills = compute_windchills(temperature, scale)
+        windchills = compute_windchills(temperature)
 
         assert windchills == [5.93, 1.42, -1.47, -3.63, -5.38, -6.85, -8.13, -9.27, -10.29, -11.22, -12.07, -12.87]
 
