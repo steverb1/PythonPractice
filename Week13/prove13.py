61cc52689b3befad45f032f1bcdaafe754c5c6f4
steverb1 <steverb@mindspring.com>
2025-04-05 09:30:04 -0600

    Fixed to match requirements

--- Week13/prove13.py
+++ Week13/prove13.py
@@ -1,12 +1,5 @@
-def compute_windchills(temperature):
-    
-    windspeed = 5
-    windspeeds = []
-    while windspeed <= 60:
-        windchill = 35.74 + 0.6215 * temperature - 35.75 * windspeed ** 0.16 + 0.4275 * temperature * windspeed ** 0.16
-        windspeeds.append(round(windchill, 2))
-        windspeed += 5
-    return windspeeds
+def compute_windchill(temperature, windspeed):
+        return 35.74 + 0.6215 * temperature - 35.75 * windspeed ** 0.16 + 0.4275 * temperature * windspeed ** 0.16
 
 def convert_celsius_to_fahrenheit(celsius):
     return round(celsius * 9 / 5 + 32, 2)
@@ -18,9 +11,8 @@ if __name__ == '__main__':
     if scale == 'C':
         temperature = convert_celsius_to_fahrenheit(temperature)
     
-    windchills = compute_windchills(temperature)
-    
     windspeed = 5
-    for windchill in windchills:
-        print(f"At temperature {temperature}F, and windspeed {windspeed} mph, the windchill is: {windchill}F")
+    while windspeed <= 60:
+        windchill = compute_windchill(temperature, windspeed)
+        print(f"At temperature {temperature}F, and windspeed {windspeed} mph, the windchill is: {windchill:.2f}F")
         windspeed += 5
